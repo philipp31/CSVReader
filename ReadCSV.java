8d0efc6e27360c40c2b2bb735dcb6bc7f1573cd5
Philipp Sle <philipp96332@hotmail.de>
2020-06-23 16:56:30 +0200

    Update ReadCSV.java

--- ReadCSV.java
+++ ReadCSV.java
@@ -9,13 +9,12 @@ public class ReadCSV {
 		
 		Scanner leser;
 		try {
-            leser = new Scanner(new File("C:/Users/philipp/Desktop/csv.txt"));
-			leser.useDelimiter(",");
+            leser = new Scanner(new File("C:/Users/philipp/Desktop/csv.txt"));	// scanner needs input, in this case file obj
+			leser.useDelimiter(",");	// for csv-format the delimiter comma is needed
             int i;
             double d;
-			int numColumns = 0;	// in the csv we have 5 columns each line!
 			
-            while (leser.hasNext()) {
+            while (leser.hasNext()) {	// if there is data left read it
                 if (leser.hasNextInt()) {
                     i = leser.nextInt();
                     System.out.println("Int: " + i);
@@ -25,7 +24,7 @@ public class ReadCSV {
                     System.out.println("Double: " + d );
 					numColumns++;
                 } else {
-				//throwing new line and carriage return away if new line beginns:
+				//throwing new line and carriage return away if new line begins:
 					char[] k = leser.next().toCharArray();	
 					for(int h = 2; h < k.length; h++) {
 						System.out.print("Int: " + k[h]);
